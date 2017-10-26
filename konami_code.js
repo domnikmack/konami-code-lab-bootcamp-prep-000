47cbef3d3818d1ad09df26f0e5694313d2941b59
Domnik Mack <domnikmack@gmail.com>
2017-10-26 19:49:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,18 +3,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   document.getElementsByTagName('body')[0].addEventListener('keydown', function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
-  let index =0;
+  let index = 0;
   if (key === code[index]) {
     index++;
-
-    if (index === alphabet.length) {
+    if (index === code.length) {
       alert("Congrats!");
-
       index = 0;
     }
   } else {
     index = 0;
   }
 });
-
 }
